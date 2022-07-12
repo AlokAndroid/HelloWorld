e4b00ed73fcba963422b0fc91f0eacbe52588314
AlokAndroid <alokstr@gmail.com>
2022-07-12 22:05:41 +0530

    Android Learning Chapter-2

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # HelloWorld
 
-Training video is available https://youtu.be/6nsbxkB3du8
+## My First App https://youtu.be/6nsbxkB3du8
 
-Training: Kotlin for android
-Chapter 1: Video like https://youtu.be/nrzvSaASjkM
+## Training: Kotlin for android
+
+### Chapter 1: Video link https://youtu.be/nrzvSaASjkM
     a. Introduction
-    merits over Java
+    merits over Java.
         1. functional
         2. less coding
         3. Thread boiler plate code reduction with coroutines which concurrently can run
@@ -17,11 +18,19 @@ Chapter 1: Video like https://youtu.be/nrzvSaASjkM
         2. Functions
         3. Classes and objects
 
-Chapter 2:
-    ...continue basic
+### Chapter 2: Video link https://youtu.be/JeMEGXyYN9I
+    ...continue basic.
         4. String format
         5. if else
         6. for loop
         7. while loop
         8. when
         9. in (Ranges) with if and for
+    c. How to access xml ui controll in code
+    #### Assignment: print Fibonacci serires on screen 
+
+### Chapter 3: 
+    c. Kotlin Exclusive
+        1. elvis operator   
+        2. exclusive return on if when
+        3. for with index
--- app/src/main/java/com/avr/training/MainActivity.kt
+++ app/src/main/java/com/avr/training/MainActivity.kt
@@ -2,19 +2,47 @@ package com.avr.training
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     val counter: Int = getCounter()
     private var message: String? = "Hello"
     var listOut: List<Int>? = null
+    var myTextView:TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         message = getMyMessage()
         listOut =  ArrayList()
+        myTextView = findViewById(R.id.my_text_input)
+        myTextView?.text = message
     }
 
     fun getMyMessage(): String{
-        return "message from function"
+        var msg = "wrong counter"
+        if(counter < 0){
+            msg = "value can not be negative"
+        }
+        else if(counter > 5){
+            msg = "value can not be more than five"
+        }
+        else{
+            msg = "value is $counter"
+        }
+        /*for(i in 0..counter){
+            msg += " $i"
+        }*/
+        var i=0
+        while(i < counter){
+            msg += " $i"
+            i++
+        }
+        when{
+            counter < 0 -> msg = "value can not be more than five"
+            counter > 5 -> msg = "value can not be more than five"
+            else -> msg = "value is $counter"
+        }
+
+        return "message from function where counter is $msg"
     }
 }
